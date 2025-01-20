116037717a5602ea5772f02b99c4fc05eb6a8ce7
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-01-20 14:19:40 -0300

    feat: Add comments

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // Cria uma instância da classe ConsultaCep
         ConsultaCep consultaCep = new ConsultaCep();
 
         System.out.println("Digite um CEP para consulta:");
         var cep = scanner.nextLine();
 
         try {
+            // Cria uma instância da classe Endereco atribuindo a instância consultaCep que
+            // chama o método buscaEndereco e passa como parâmetro o CEP
             Endereco novoEndereco = consultaCep.buscaEndereco(cep);
             System.out.println(novoEndereco);
+            // Gera o arquivo Json
             GeradorDeArquivo generate = new GeradorDeArquivo();
             generate.salvaJson(novoEndereco);
         } catch (RuntimeException | IOException e) {
